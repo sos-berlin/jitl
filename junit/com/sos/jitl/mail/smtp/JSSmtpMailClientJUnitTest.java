6a74a425a4eaf9042a220701e5a9c1a0f62968f2
Klaus Büttner <klaus.buettner.git@sos-berlin.com>
2013-07-18 18:44:45 +0000

    -
    svn-id 20727

--- junit/com/sos/jitl/mail/smtp/JSSmtpMailClientJUnitTest.java
+++ junit/com/sos/jitl/mail/smtp/JSSmtpMailClientJUnitTest.java
@@ -69,8 +69,8 @@ public class JSSmtpMailClientJUnitTest extends JSToolBox {
 
 		objOptions.from.Value("JUnit-Test@sos-berlin.com");
 
-		objOptions.body.Value( "bodobodododo\nfjfjfjfjfjjf\nfkfkfkfkfkfkf\nfjfjfjfjfjf");
-//		objOptions.subject.Value( "mail from JSSmtpMailClientJUnitTest");
+		objOptions.body.Value( "bodobodododo\nfjfjfjfjfjjf\nfkfkfkfkfkfkf\nfjfjfjfjfjf\n %{host}  ");
+		objOptions.subject.Value( "mail from JSSmtpMailClientJUnitTest %{host}\n  date = %{date}\n");
 		objOptions.to.Value("kb@sos-berlin.com");
 		objOptions.cc.Value("kb@sos-berlin.com;info@sos-berlin.com");
 		objOptions.bcc.Value("kb@sos-berlin.com;support@sos-berlin.com");
@@ -79,5 +79,6 @@ public class JSSmtpMailClientJUnitTest extends JSToolBox {
 	}
 
 
+	
 
 } // class JSSmtpMailClientJUnitTest
