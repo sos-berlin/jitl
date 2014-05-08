4b0ba12fbd7c24a9d6a2377944438742baddf212
Santiago Aucejo Petzoldt <santiago.aucejo.petzoldt.git@sos-berlin.com>
2014-05-08 11:05:13 +0000

    throws Exception removed from overriding Methods [SP]
    svn-id 24250

--- src/com/sos/jitl/sync/JobSchedulerSynchronizeJobChainsOptionsSuperClass.java
+++ src/com/sos/jitl/sync/JobSchedulerSynchronizeJobChainsOptionsSuperClass.java
@@ -628,7 +628,7 @@ public class JobSchedulerSynchronizeJobChainsOptionsSuperClass extends JSOptions
      * @throws Exception
      */
     @Override
-    public void setAllOptions(final HashMap<String, String> pobjJSSettings) throws Exception {
+    public void setAllOptions(final HashMap<String, String> pobjJSSettings) {
         @SuppressWarnings("unused")
         final String conMethodName = conClassName + "::setAllOptions";
         flgSetAllOptions = true;
@@ -673,7 +673,7 @@ public class JobSchedulerSynchronizeJobChainsOptionsSuperClass extends JSOptions
      * @throws Exception
      */
     @Override
-    public void CommandLineArgs(final String[] pstrArgs) throws Exception {
+    public void CommandLineArgs(final String[] pstrArgs) {
         super.CommandLineArgs(pstrArgs);
         this.setAllOptions(super.objSettings);
     }
