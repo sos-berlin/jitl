e92797e1ce3d3ee606fd467f10156874fceaac79
Klaus Büttner <klaus.buettner.git@sos-berlin.com>
2014-08-18 11:43:28 +0000

    -
    svn-id 26791

--- jitl-jobs/src/test/java/com/sos/jitl/mail/smtp/JSSmtpMailClientJUnitTest.java
+++ jitl-jobs/src/test/java/com/sos/jitl/mail/smtp/JSSmtpMailClientJUnitTest.java
@@ -1,9 +1,14 @@
 package com.sos.jitl.mail.smtp;
 
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import com.sos.JSHelper.Basics.JSToolBox;
 import com.sos.JSHelper.Listener.JSListenerClass;
-import org.apache.log4j.Logger;
-import org.junit.*;
 
 /**
  * \class 		JSSmtpMailClientJUnitTest - JUnit-Test for "Submit and Delete Events"
@@ -61,9 +66,9 @@ public class JSSmtpMailClientJUnitTest extends JSToolBox {
 
 		objOptions.body.Value( "bodobodododo\nfjfjfjfjfjjf\nfkfkfkfkfkfkf\nfjfjfjfjfjf\n %{host}  ");
 		objOptions.subject.Value( "mail from JSSmtpMailClientJUnitTest %{host}\n  date = %{date}\n");
-		objOptions.to.Value("kb@sos-berlin.com");
-		objOptions.cc.Value("kb@sos-berlin.com;info@sos-berlin.com");
-		objOptions.bcc.Value("kb@sos-berlin.com;scheduler_test@sos-berlin.com");
+		objOptions.to.Value("scheduler_test@sos-berlin.com");
+		objOptions.cc.Value("info@sos-berlin.com");
+		objOptions.bcc.Value("support@sos-berlin.com");
 
 		objE.Execute();
 	}
