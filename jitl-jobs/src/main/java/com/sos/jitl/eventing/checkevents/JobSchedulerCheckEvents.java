942c7c84452d7a95f8b4d8cd43f17d8c2ac39a07
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2015-10-28 12:45:55 +0100

    JITL-191

--- jitl-jobs/src/main/java/com/sos/jitl/eventing/checkevents/JobSchedulerCheckEvents.java
+++ jitl-jobs/src/main/java/com/sos/jitl/eventing/checkevents/JobSchedulerCheckEvents.java
@@ -1,6 +1,4 @@
 package com.sos.jitl.eventing.checkevents;
-import java.io.File;
-
 import org.apache.log4j.Logger;
 
 import com.sos.JSHelper.Basics.JSJobUtilities;
@@ -22,79 +20,34 @@ import com.sos.eventing.db.SchedulerEventFilter;
  * \endverbatim
  */
 public class JobSchedulerCheckEvents extends JSJobUtilitiesClass<JobSchedulerCheckEventsOptions> implements JSJobUtilities {
-	@SuppressWarnings("unused") private final String conClassName = this.getClass().getSimpleName();
-	@SuppressWarnings("unused") private static final String conSVNVersion = "$Id$";
-	@SuppressWarnings("unused") private final Logger logger = Logger.getLogger(this.getClass());
-	
-
-//	protected JobSchedulerCheckEventsOptions	objOptions			= null;
-//	private JSJobUtilities						objJSJobUtilities	= this;
+	private final Logger logger = Logger.getLogger(JobSchedulerCheckEvents.class);
 	protected boolean							exist				= false;
   
-	
-	/**
-	 * 
-	 * \brief JobSchedulerCheckEvents
-	 *
-	 * \details
-	 *
-	 */
 	public JobSchedulerCheckEvents() {
 		super();
 	}
 
-	/**
-	 * 
-	 * \brief Options - returns the JobSchedulerCheckEventsOptionClass
-	 * 
-	 * \details
-	 * The JobSchedulerCheckEventsOptionClass is used as a Container for all Options (Settings) which are
-	 * needed.
-	 *  
-	 * \return JobSchedulerCheckEventsOptions
-	 *
-	 */
 	@Override
 	public JobSchedulerCheckEventsOptions getOptions() {
-		@SuppressWarnings("unused")
-		final String conMethodName = conClassName + "::Options"; //$NON-NLS-1$
 		if (objOptions == null) {
 			objOptions = new JobSchedulerCheckEventsOptions();
 		}
 		return objOptions;
 	}
 
-	/**
-	 * 
-	 * \brief Execute - Start the Execution of JobSchedulerCheckEvents
-	 * 
-	 * \details
-	 * 
-	 * For more details see
-	 * 
-	 * \see JobSchedulerAdapterClass 
-	 * \see JobSchedulerCheckEventsMain
-	 * 
-	 * \return JobSchedulerCheckEvents
-	 *
-	 * @return
-	 */
 	public JobSchedulerCheckEvents Execute() throws Exception {
-		final String conMethodName = conClassName + "::Execute"; //$NON-NLS-1$
 		try {
 			getOptions().CheckMandatory();
 			logger.debug(getOptions().toString());
 			exist = false;
-			SchedulerEventDBLayer schedulerEventDBLayer = new SchedulerEventDBLayer(new File(objOptions.configuration_file.Value()));
+			SchedulerEventDBLayer schedulerEventDBLayer = new SchedulerEventDBLayer(objOptions.configuration_file.Value());
 			if (objOptions.event_condition.isDirty()) {
 				if (objOptions.event_class.isDirty()) {
 					exist = schedulerEventDBLayer.checkEventExists(objOptions.event_condition.Value(), objOptions.event_class.Value());
-				}
-				else {
+				} else {
 					exist = schedulerEventDBLayer.checkEventExists(objOptions.event_condition.Value());
 				}
-			}
-			else {
+			} else {
 				SchedulerEventFilter schedulerEventFilter = new SchedulerEventFilter();
 				schedulerEventFilter.setEventClass(objOptions.event_class.Value());
 				schedulerEventFilter.setEventId(objOptions.event_id.Value());
@@ -107,24 +60,12 @@ public class JobSchedulerCheckEvents extends JSJobUtilitiesClass<JobSchedulerChe
 				schedulerEventFilter.setJobName(objOptions.event_job.Value());
 				exist = schedulerEventDBLayer.checkEventExists(schedulerEventFilter);
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace(System.err);
 			throw e;
 		}
-		finally {
-		}
 		return this;
 	}
-
-	public void init() {
-		@SuppressWarnings("unused")
-		final String conMethodName = conClassName + "::init"; //$NON-NLS-1$
-		doInitialize();
-	}
-
-	private void doInitialize() {
-	} // doInitialize
 
 	@Override
 	public String myReplaceAll(final String pstrSourceString, final String pstrReplaceWhat, final String pstrReplaceWith) {
@@ -144,7 +85,8 @@ public class JobSchedulerCheckEvents extends JSJobUtilitiesClass<JobSchedulerChe
 	 * @param pstrString2Modify
 	 * @return
 	 */
-	@Override public String replaceSchedulerVars(final boolean isWindows, final String pstrString2Modify) {
+	@Override 
+	public String replaceSchedulerVars(final boolean isWindows, final String pstrString2Modify) {
 		logger.debug("replaceSchedulerVars as Dummy-call executed. No Instance of JobUtilites specified.");
 		return pstrString2Modify;
 	}
@@ -154,16 +96,18 @@ public class JobSchedulerCheckEvents extends JSJobUtilitiesClass<JobSchedulerChe
 	 * \brief setJSParam
 	 * 
 	 * \details
-	 * Dummy-Method to make shure, that there is always a valid Instance for the JSJobUtilities.
+	 * Dummy-Method to make sure, that there is always a valid Instance for the JSJobUtilities.
 	 * \return 
 	 *
 	 * @param pstrKey
 	 * @param pstrValue
 	 */
-	@Override public void setJSParam(final String pstrKey, final String pstrValue) {
+	@Override 
+	public void setJSParam(final String pstrKey, final String pstrValue) {
 	}
 
-	@Override public void setJSParam(final String pstrKey, final StringBuffer pstrValue) {
+	@Override 
+	public void setJSParam(final String pstrKey, final StringBuffer pstrValue) {
 	}
 
 	/**
@@ -189,22 +133,21 @@ public class JobSchedulerCheckEvents extends JSJobUtilitiesClass<JobSchedulerChe
 		logger.debug("objJSJobUtilities = " + objJSJobUtilities.getClass().getName());
 	}
 
-	@Override public String getCurrentNodeName() {
-		// TODO Auto-generated method stub
+	@Override 
+	public String getCurrentNodeName() {
 		return null;
 	}
 
-	@Override public void setStateText(final String pstrStateText) {
-		// TODO Auto-generated method stub
+	@Override 
+	public void setStateText(final String pstrStateText) {
 	}
 
-	@Override public void setCC(final int pintCC) {
-		// TODO Auto-generated method stub
+	@Override 
+	public void setCC(final int pintCC) {
 	}
 
     @Override
     public void setNextNodeState(String pstrNodeName) {
-        // TODO Auto-generated method stub
-        
     }
+    
 } // class JobSchedulerCheckEvents
--- jitl-jobs/src/main/java/com/sos/jitl/housekeeping/cleanupdb/JobSchedulerCleanupSchedulerDb.java
+++ jitl-jobs/src/main/java/com/sos/jitl/housekeeping/cleanupdb/JobSchedulerCleanupSchedulerDb.java
@@ -1,8 +1,5 @@
 package com.sos.jitl.housekeeping.cleanupdb;
 
-import java.io.File;
-import java.sql.Connection;
-
 import org.apache.log4j.Logger;
 
 import sos.jadehistory.db.JadeFilesDBLayer;
@@ -12,8 +9,6 @@ import com.sos.JSHelper.Basics.JSJobUtilities;
 import com.sos.JSHelper.Basics.JSJobUtilitiesClass;
 import com.sos.JSHelper.Exceptions.JobSchedulerException;
 import com.sos.dailyschedule.db.DailyScheduleDBLayer;
-import com.sos.hibernate.classes.SosHibernateSession;
-import com.sos.hibernate.options.HibernateOptions;
 import com.sos.scheduler.history.db.SchedulerOrderHistoryDBLayer;
 import com.sos.scheduler.history.db.SchedulerTaskHistoryDBLayer;
 import com.sos.scheduler.messages.JSMessages;
@@ -25,142 +20,70 @@ public class JobSchedulerCleanupSchedulerDb extends JSJobUtilitiesClass<JobSched
 	protected JobSchedulerCleanupSchedulerDbOptions	objOptions			= null;
 	private final JSJobUtilities							objJSJobUtilities	= this;
 
-	/**
-	 *
-	 * \brief JobSchedulerCleanupSchedulerDb
-	 *
-	 * \details
-	 *
-	 */
 	public JobSchedulerCleanupSchedulerDb() {
 		super(new JobSchedulerCleanupSchedulerDbOptions());
 	}
 
-	/**
-	 *
-	 * \brief Options - returns the JobSchedulerCleanupSchedulerDbOptionClass
-	 *
-	 * \details
-	 * The JobSchedulerCleanupSchedulerDbOptionClass is used as a Container for all Options (Settings) which are
-	 * needed.
-	 *
-	 * \return JobSchedulerCleanupSchedulerDbOptions
-	 *
-	 */
 	@Override
 	public JobSchedulerCleanupSchedulerDbOptions getOptions() {
-
-		@SuppressWarnings("unused")
-		final String conMethodName = conClassName + "::Options"; //$NON-NLS-1$
-
 		if (objOptions == null) {
 			objOptions = new JobSchedulerCleanupSchedulerDbOptions();
 		}
 		return objOptions;
 	}
 
-	/**
-	 *
-	 * \brief Execute - Start the Execution of JobSchedulerCleanupSchedulerDb
-	 *
-	 * \details
-	 *
-	 * For more details see
-	 *
-	 * \see JobSchedulerAdapterClass
-	 * \see JobSchedulerCleanupSchedulerDbMain
-	 *
-	 * \return JobSchedulerCleanupSchedulerDb
-	 *
-	 * @return
-	 */
 	public JobSchedulerCleanupSchedulerDb Execute() throws Exception {
 		final String conMethodName = conClassName + "::Execute"; //$NON-NLS-1$
-         
-        
-        
 		logger.debug(String.format(JSMessages.JSJ_I_110.get(), conMethodName));
-
 		try {
 			getOptions().CheckMandatory();
 			logger.debug(getOptions().dirtyString());
-
 		 	if (getOptions().cleanup_job_scheduler_history_execute.isTrue()) {
-    			SchedulerOrderHistoryDBLayer schedulerOrderHistoryDBLayer = new SchedulerOrderHistoryDBLayer(new File(getOptions().hibernate_configuration_file.Value()));
+    			SchedulerOrderHistoryDBLayer schedulerOrderHistoryDBLayer = new SchedulerOrderHistoryDBLayer(getOptions().hibernate_configuration_file.Value());
     			if (!getOptions().delete_history_interval.isDirty()) {
     				getOptions().delete_history_interval.Value(getOptions().delete_interval.Value());
     			}
-     			
-                schedulerOrderHistoryDBLayer.beginTransaction(Connection.TRANSACTION_READ_UNCOMMITTED);
     			long i = schedulerOrderHistoryDBLayer.deleteInterval(getOptions().delete_history_interval.value(),getOptions().cleanup_jobscheduler_history_limit.value());
-             
     			logger.info(String.format("%s records deleted from SCHEDULER_ORDER_HISTORY that are older than %s days", i, getOptions().delete_history_interval.Value()));
-    
-    			SchedulerTaskHistoryDBLayer schedulerTaskHistoryDBLayer = new SchedulerTaskHistoryDBLayer(new File(getOptions().hibernate_configuration_file.Value()));
-    			schedulerTaskHistoryDBLayer.beginTransaction(Connection.TRANSACTION_READ_UNCOMMITTED);
+    			SchedulerTaskHistoryDBLayer schedulerTaskHistoryDBLayer = new SchedulerTaskHistoryDBLayer(getOptions().hibernate_configuration_file.Value());
     			i = schedulerTaskHistoryDBLayer.deleteInterval(getOptions().delete_history_interval.value(),getOptions().cleanup_jobscheduler_history_limit.value());
-    
     			logger.info(String.format("%s records deleted from SCHEDULER_HISTORY that are older than %s days", i, getOptions().delete_history_interval.Value()));
 			}else {
                 logger.info("Records in SCHEDULER_ORDER_HISTORY and SCHEDULER_HISTORY will not be deleted");
 			}
-			 
-          if (getOptions().cleanup_daily_plan_execute.isTrue()) {
-    			DailyScheduleDBLayer dailyScheduleDBLayer = new DailyScheduleDBLayer(new File(getOptions().hibernate_configuration_file.Value()));
-                dailyScheduleDBLayer.beginTransaction(Connection.TRANSACTION_READ_UNCOMMITTED);
-    			if (!getOptions().delete_daily_plan_interval.isDirty()) {
-    				getOptions().delete_daily_plan_interval.Value(getOptions().delete_interval.Value());
-    			}
+		 	if (getOptions().cleanup_daily_plan_execute.isTrue()) {
+		 		DailyScheduleDBLayer dailyScheduleDBLayer = new DailyScheduleDBLayer(getOptions().hibernate_configuration_file.Value());
+		 		if (!getOptions().delete_daily_plan_interval.isDirty()) {
+		 			getOptions().delete_daily_plan_interval.Value(getOptions().delete_interval.Value());
+		 		}
     			long i = dailyScheduleDBLayer.deleteInterval(getOptions().delete_daily_plan_interval.value(),getOptions().cleanup_daily_plan_limit.value());
     			logger.info(String.format("%s records deleted from DAYS_SCHEDULE that are older than %s days", i, getOptions().delete_history_interval.Value()));
-            }else {
+            } else {
                 logger.info("Records in DAYS_SCHEDULE will not be deleted");
             }
-			 
             if (getOptions().cleanup_jade_history_execute.isTrue()) {
-                JadeFilesDBLayer jadeFilesDBLayer = new JadeFilesDBLayer(new File(getOptions().hibernate_configuration_file.Value()));
-                jadeFilesDBLayer.beginTransaction(Connection.TRANSACTION_READ_UNCOMMITTED);
+                JadeFilesDBLayer jadeFilesDBLayer = new JadeFilesDBLayer(getOptions().hibernate_configuration_file.Value());
     			if (!getOptions().delete_jade_history_interval.isDirty()) {
     				getOptions().delete_jade_history_interval.Value(getOptions().delete_interval.Value());
     			}
     			long i = jadeFilesDBLayer.deleteInterval(getOptions().delete_jade_history_interval.value(),getOptions().cleanup_jade_history_limit.value());
     			logger.info(String.format("%s records deleted from JADE_FILES that are older than %s days", i, getOptions().delete_jade_history_interval.Value()));
-    			
-                JadeFilesHistoryDBLayer jadeFilesHistoryDBLayer = new JadeFilesHistoryDBLayer(new File(getOptions().hibernate_configuration_file.Value()));
-                jadeFilesHistoryDBLayer.beginTransaction(Connection.TRANSACTION_READ_UNCOMMITTED);
+                JadeFilesHistoryDBLayer jadeFilesHistoryDBLayer = new JadeFilesHistoryDBLayer(getOptions().hibernate_configuration_file.Value());
                 if (!getOptions().delete_jade_history_interval.isDirty()) {
                     getOptions().delete_jade_history_interval.Value(getOptions().delete_interval.Value());
                 }
-                
                 i = jadeFilesHistoryDBLayer.deleteInterval(getOptions().delete_jade_history_interval.value(),getOptions().cleanup_jade_history_limit.value());
                 logger.info(String.format("%s records deleted from JADE_FILES_HISTORY that are older than %s days", i, getOptions().delete_jade_history_interval.Value()));
-
             }else {
                 logger.info("Records in JADE_FILES will not be deleted");
             }
-
-			 
-
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			String strM = String.format(JSMessages.JSJ_F_107.get(), conMethodName);
 			throw new JobSchedulerException(strM, e);
-		}
-		finally {
-		    SosHibernateSession.close();
+		} finally {
 			logger.debug(String.format(JSMessages.JSJ_I_111.get(), conMethodName));
 		}
-
 		return this;
 	}
-
-	public void init() {
-		@SuppressWarnings("unused")
-		final String conMethodName = conClassName + "::init"; //$NON-NLS-1$
-		doInitialize();
-	}
-
-	private void doInitialize() {
-	} // doInitialize
 
 } // class JobSchedulerCleanupSchedulerDb
