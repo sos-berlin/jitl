f9faa5c409d7472dc3bb45c0978c86cbd39473fa
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2016-06-03 10:59:30 +0200

    JITL-240: test corrected

--- jitl-jobs/src/test/java/com/sos/scheduler/messages/JSMsgTest.java
+++ jitl-jobs/src/test/java/com/sos/scheduler/messages/JSMsgTest.java
@@ -1,58 +1,39 @@
 package com.sos.scheduler.messages;
 
-import com.sos.localization.SOSMsg;
-import org.apache.log4j.Logger;
-import org.junit.*;
-import com.sos.jitl.checkrunhistory.JobSchedulerCheckRunHistoryJUnitTest;
-
 import static com.sos.scheduler.messages.JSMessages.JSJ_D_0032;
 import static com.sos.scheduler.messages.JSMessages.JSJ_I_110;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sos.localization.SOSMsg;
+
 public class JSMsgTest {
 
-    @SuppressWarnings("unused")
-    private final static String conClassName = "JobSchedulerCheckRunHistoryJUnitTest";						//$NON-NLS-1$
-    @SuppressWarnings("unused")
-    private static Logger logger = Logger.getLogger(JobSchedulerCheckRunHistoryJUnitTest.class);
-
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
+    private static final String CLASSNAME = "JSMsgTest";
 
     @Before
     public void setUp() throws Exception {
         SOSMsg.flgShowFullMessageText = true;
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test(expected = com.sos.JSHelper.Exceptions.JobSchedulerException.class)
     public void testJSMsgString() {
         JSMsg objMsg = new JSMsg("JSJ_F_107");
-        objMsg.toLog(conClassName);
+        objMsg.toLog(CLASSNAME);
     }
 
     @Test
     public void testJSMsg_I_110() {
-        JSJ_I_110.toLog(conClassName);
+        JSJ_I_110.toLog(CLASSNAME);
     }
 
     @Test
     public void testJSMsg_D_032() {
         JSMsg.VerbosityLevel = 5;
-        JSJ_D_0032.toLog(conClassName);
+        JSJ_D_0032.toLog(CLASSNAME);
         JSMsg.VerbosityLevel = 1;
-        JSJ_D_0032.toLog(conClassName);
-    }
-
-    @Test
-    public void testJSMsgStringInt() {
+        JSJ_D_0032.toLog(CLASSNAME);
     }
 
 }
