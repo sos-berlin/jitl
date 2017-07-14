85c1f6a916046a4fc22a2a1b29390c0ddb983d3c
Uwe Risse <uwe.risse-git@sos-berlin.com>
2017-07-14 15:21:54 +0200

    JOC-126

--- jitl-jobs/src/main/java/com/sos/jitl/dailyplan/db/DailyPlanInterval.java
+++ jitl-jobs/src/main/java/com/sos/jitl/dailyplan/db/DailyPlanInterval.java
@@ -2,10 +2,20 @@ package com.sos.jitl.dailyplan.db;
 
 import java.util.Date;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sos.hibernate.classes.UtcTimeHelper;
+
 public class DailyPlanInterval {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DailyPlanInterval.class);
     private Date from;
     private Date to;
+    private Date convertedFrom;
+    private Date convertedTo;
 
     public DailyPlanInterval(Date from, Date to) {
         super();
@@ -19,6 +29,39 @@ public class DailyPlanInterval {
 
     public Date getTo() {
         return to;
+    }
+
+    public Date getConvertedFrom() {
+        if (convertedFrom == null){
+            convert2Utc();
+        }
+        return convertedFrom;
+    }
+
+    public Date getConvertedTo() {
+        if (convertedTo == null){
+            convert2Utc();
+        }
+        return convertedTo;
+    }
+
+    private void convert2Utc() {
+        String toTimeZoneString = "UTC";
+
+        String fromTimeZoneString = DateTimeZone.getDefault().getID();
+        LOGGER.debug("fromTimeZone:" + fromTimeZoneString);
+        LOGGER.debug("toTimeZone:" + toTimeZoneString);
+        LOGGER.debug("intervall from:" + this.getFrom());
+        LOGGER.debug("intervall to:" + this.getTo());
+        DateTime utcFrom = new DateTime(this.getFrom()).withZone(DateTimeZone.UTC);
+        utcFrom = utcFrom.withZone(DateTimeZone.UTC);
+        DateTime utcTo = new DateTime(this.getTo()).withZone(DateTimeZone.UTC);
+        utcTo = utcTo.withZone(DateTimeZone.UTC);
+
+        convertedFrom = UtcTimeHelper.convertTimeZonesToDate(fromTimeZoneString, toTimeZoneString, utcFrom);
+        convertedTo = UtcTimeHelper.convertTimeZonesToDate(fromTimeZoneString, toTimeZoneString, new DateTime(utcTo));
+        LOGGER.debug("converted intervall from:" + convertedFrom);
+        LOGGER.debug("convertet intervall to:" + convertedTo);
     }
 
 }
