e4c5454a09b551f528600f993a1d42c9047faabf
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2017-08-22 13:06:23 +0200

    JOC-268

--- jitl-jobs/src/main/java/com/sos/jitl/inventory/helper/Calendar2DBHelper.java
+++ jitl-jobs/src/main/java/com/sos/jitl/inventory/helper/Calendar2DBHelper.java
@@ -12,13 +12,13 @@ public class Calendar2DBHelper {
 
     private static final String WEBSERVICE_COMMAND_URL = "/jobscheduler/master/api/command";
 
-    public static Calendar2DB initCalendar2Db (DBLayerInventory dbLayer, DBItemInventoryInstance instance) throws Exception {
+    public static Calendar2DB initCalendar2Db (DBLayerInventory dbLayer, DBItemInventoryInstance instance, String httpHost
+            , Integer httpPort) throws Exception {
         Calendar2DB calendar2Db = new Calendar2DB(dbLayer.getSession(), instance.getSchedulerId());
         HashMap<String, String> createDaysScheduleOptionsMap = new HashMap<String, String>();
-        String commandUrl = instance.getUrl() + WEBSERVICE_COMMAND_URL;
-        if (instance.getAuth() != null && !instance.getAuth().isEmpty()) {
-            createDaysScheduleOptionsMap.put("basic_authorization", instance.getAuth());
-        }
+        StringBuilder strb = new StringBuilder();
+        strb.append("http://").append(httpHost).append(":").append(httpPort).append(WEBSERVICE_COMMAND_URL);
+        String commandUrl = strb.toString();
         createDaysScheduleOptionsMap.put("command_url", commandUrl);
         CreateDailyPlanOptions options = new CreateDailyPlanOptions();
         options.setAllOptions(createDaysScheduleOptionsMap);
