e41100851c760d1fc565a32b28303de61dac6780
Uwe Risse <uwe.risse-git@sos-berlin.com>
2018-01-30 17:47:50 +0100

    JITL-439

--- jitl-jobs/src/main/java/sos/scheduler/job/JobSchedulerDequeueMail.java
+++ jitl-jobs/src/main/java/sos/scheduler/job/JobSchedulerDequeueMail.java
@@ -1,21 +1,9 @@
 package sos.scheduler.job;
 
-/** @author andreas pueschel */
-public class JobSchedulerDequeueMail extends JobSchedulerJobAdapter {
-
-    private int numOfMails = 0;
-
-    public boolean spooler_open() {
-        this.numOfMails = spooler_log.mail().dequeue();
-        return this.numOfMails > 0;
-    }
-
-    public boolean spooler_process() {
-        spooler_log.info(".......extending JobSchedulderAdapter");
-        if (this.numOfMails > 0) {
-            spooler_log.info(this.numOfMails + " previously stored mails were sent, mail queue is empty");
-        }
-        return false;
+public class JobSchedulerDequeueMail extends com.sos.jitl.housekeeping.dequeuemail.JobSchedulerResendFailedDequeuedMailsAdapterClass {
+   
+    public boolean spooler_process() throws Exception {
+        return super.spooler_process();
     }
 
 }
