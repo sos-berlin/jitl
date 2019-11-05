c53a2116c1351944613aefe1014f4241a6262041
SOS-RE <robert.ehrlich@sos-berlin.com>
2019-11-05 17:12:18 +0100

    JOC-731 DeleteDraft

--- jitl-jobs/src/main/java/com/sos/jitl/xmleditor/db/DbLayerXmlEditor.java
+++ jitl-jobs/src/main/java/com/sos/jitl/xmleditor/db/DbLayerXmlEditor.java
@@ -8,11 +8,29 @@ import org.hibernate.query.Query;
 import com.sos.hibernate.classes.SOSHibernateSession;
 import com.sos.jitl.reporting.db.DBItemXmlEditorObject;
 import com.sos.jitl.reporting.db.DBLayer;
+import com.sos.joc.model.xmleditor.common.ObjectType;
 
 public class DbLayerXmlEditor extends DBLayer {
 
     public DbLayerXmlEditor(SOSHibernateSession session) {
         super(session);
+    }
+
+    public boolean deleteOtherObject(Long id) throws Exception {
+        // JOC uses autoCommit=true, executeUpdate is not supported
+        StringBuilder hql = new StringBuilder("from ").append(DBITEM_XML_EDITOR_OBJECTS).append(" ");
+        hql.append("where id=:id ");
+        hql.append("and objectType=:objectType");
+        Query<DBItemXmlEditorObject> query = getSession().createQuery(hql.toString());
+        query.setParameter("id", id);
+        query.setParameter("objectType", ObjectType.OTHER.name());
+
+        DBItemXmlEditorObject item = getSession().getSingleResult(query);
+        if (item != null) {
+            getSession().delete(item);
+            return true;
+        }
+        return false;
     }
 
     public DBItemXmlEditorObject getObject(Long id) throws Exception {
@@ -34,7 +52,6 @@ public class DbLayerXmlEditor extends DBLayer {
         query.setParameter("schedulerId", schedulerId);
         query.setParameter("objectType", objectType);
         query.setParameter("name", name);
-
         return getSession().getSingleResult(query);
     }
 
