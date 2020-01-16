982513a185d7b95eb18307bc82398d62a4532e63
SOS-RE <robert.ehrlich@sos-berlin.com>
2020-01-16 12:20:30 +0100

    JITL-589 reset changes

--- jitl-jobs/src/main/java/com/sos/jitl/inventory/db/InventoryCalendarUpdate.java
+++ jitl-jobs/src/main/java/com/sos/jitl/inventory/db/InventoryCalendarUpdate.java
@@ -9,8 +9,6 @@ import com.sos.hibernate.classes.SOSHibernateFactory;
 import com.sos.hibernate.classes.SOSHibernateSession;
 import com.sos.jitl.reporting.db.DBLayer;
 
-import sos.util.SOSString;
-
 public class InventoryCalendarUpdate {
 
     public static void main(String[] args) {
@@ -19,14 +17,7 @@ public class InventoryCalendarUpdate {
             SOSHibernateFactory factory = null;
             boolean calledFromSetup = false;
             if (args.length > 1 && "-execute-from-setup".equals(args[1])) {
-                calledFromSetup = true; 
-            }
-            //JITL-589 - scheduler_intstall_tables script and relative paths in the hibernate configuration file
-            String ud = null;
-            String appdata = System.getenv("APPDATA_PATH");
-            if (!SOSString.isEmpty(appdata)) {
-                ud = System.getProperty("user.dir");
-                System.setProperty("user.dir", appdata);
+                calledFromSetup = true;
             }
             try {
                 if (calledFromSetup) {
@@ -38,9 +29,9 @@ public class InventoryCalendarUpdate {
                 }
                 factory = new SOSHibernateFactory(hibernateConfigPath);
                 factory.setAutoCommit(false);
-                //factory.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+                // factory.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
                 factory.addClassMapping(DBLayer.getInventoryClassMapping());
-                
+
                 factory.build();
                 connection = factory.openStatelessSession(InventoryCalendarUpdate.class.getName());
                 connection.beginTransaction();
@@ -62,9 +53,6 @@ public class InventoryCalendarUpdate {
                 }
                 System.exit(1);
             } finally {
-                if (!SOSString.isEmpty(ud)) {
-                    System.setProperty("user.dir", ud);
-                }
                 if (connection != null) {
                     connection.close();
                 }
