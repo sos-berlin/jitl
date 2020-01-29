c163654a7da1bfce917ac1bbb14bea7541320241
uwe-risse-sos <uwe.risse@sos-berlin.com>
2020-01-29 12:21:35 +0100

    JITL-583

--- jitl-jobs/src/test/resources/com/sos/jitl/JobchainNodeSubstitute/xjc.cmd
+++ jitl-jobs/src/test/resources/com/sos/jitl/JobchainNodeSubstitute/xjc.cmd
@@ -1,2 +1,4 @@
-"C:\Program Files\Java\jdk1.8.0_31\bin\xjc.exe" -p com.sos.jitl.configurationmonitor.model  -d C:\development\products\jitl\jitl-jobs\src\main\java\ "C:\development\products\jitl\jitl-jobs\src\test\resources\com\sos\jitl\configurationmonitor\jobchain_config.xsd"
+"C:\Program Files\Java\jdk1.8.0_171\bin\xjc.exe" -p com.sos.jitl.jobchainnodeparameter.model  -d d:\development_113\products\jitl\jitl-jobs\src\main\java\ "d:\development_113\products\jitl\jitl-jobs\src\test\resources\com\sos\jitl\JobchainNodeSubstitute\jobchain.config.xsd"
 pause
+
+
