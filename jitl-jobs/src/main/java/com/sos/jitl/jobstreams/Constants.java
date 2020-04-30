cb6d59163bd0eda662e89dbcacd237dfc4b4b470
uwe-risse-sos <uwe.risse@sos-berlin.com>
2020-04-30 13:02:32 +0200

    JOC-919

--- jitl-jobs/src/main/java/com/sos/jitl/jobstreams/Constants.java
+++ jitl-jobs/src/main/java/com/sos/jitl/jobstreams/Constants.java
@@ -4,8 +4,11 @@ import java.util.Calendar;
 
 import com.sos.hibernate.classes.ClassList;
 import com.sos.jitl.eventhandler.handler.EventHandlerSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;  
 
 public class Constants {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Constants.class);
 
     public static enum OutConditionEventCommand {
         create, delete
@@ -36,13 +39,69 @@ public class Constants {
     public static final String IN_CONDITION_COMMANDS_TABLE = "JSTREAM_IN_CONDITION_COMMANDS";
 
     public static EventHandlerSettings settings = null;
-    public static String baseUrl;
+    public static String baseUrl;   
+    public static String periodBegin;
+
+    public static Calendar getSessionCalendar() {
+        if (periodBegin == null) {
+            periodBegin = "00:00";
+        }
+        Calendar calendar = Calendar.getInstance();
+        String[] period = periodBegin.split(":");
+        int hours = 0;
+        int minutes = 0;
+        if (period.length == 1) {
+            try {
+                hours = Integer.parseInt(period[0]);
+            } catch (NumberFormatException e) {
+                LOGGER.warn("Wrong time format for sos.jobstream_period_begin: " + periodBegin);
+                hours = 0;
+                minutes = 0;
+                periodBegin = "00:00";
+            }
+        }
+        if (period.length == 2) {
+            try {
+                hours = Integer.parseInt(period[0]);
+                minutes = Integer.parseInt(period[1]);
+            } catch (NumberFormatException e) {
+                LOGGER.warn("Wrong time format for sos.jobstream_period_begin: " + periodBegin);
+                hours = 0;
+                minutes = 0;
+                periodBegin = "00:00";
+            }
+        }
+        if (hours < 0) {
+            hours = hours * -1;
+            hours = hours + 24;
+        }
+
+        if (hours == 0) {
+            hours = 24;
+        }
+        if (minutes == 0) {
+            minutes = 60;
+        }
+        hours = (24 - hours);
+        minutes = (60 - minutes);
+
+        calendar.add(Calendar.HOUR_OF_DAY, hours);
+        calendar.add(Calendar.HOUR_OF_DAY, minutes);
+
+        return calendar;
+    }
 
     public static String getSession() {
-        Calendar calendar = Calendar.getInstance();
+        Calendar calendar = getSessionCalendar();
+ 
         int month = calendar.get(Calendar.MONTH) + 1;
         int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-        return String.valueOf(month) + "." + String.valueOf(dayOfMonth);
+
+        LOGGER.debug("Period starts at  " + periodBegin + " (UTC)");
+        String result = String.valueOf(month) + "." + String.valueOf(dayOfMonth);
+        LOGGER.debug("period session is: " + result);
+
+        return result;
     }
 
     public static ClassList getConditionsClassMapping() {
