894a94143a41f6183f8201e992d56016fcdd7b0a
uwe-risse-sos <uwe.risse@sos-berlin.com>
2020-06-04 12:55:36 +0200

    JOC-912

--- jitl-jobs/src/main/java/com/sos/jitl/jobstreams/db/DBLayerJobStreamStarters.java
+++ jitl-jobs/src/main/java/com/sos/jitl/jobstreams/db/DBLayerJobStreamStarters.java
@@ -1,5 +1,8 @@
 package com.sos.jitl.jobstreams.db;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -7,19 +10,27 @@ import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sos.hibernate.classes.SOSHibernateSession;
 import com.sos.hibernate.exceptions.SOSHibernateException;
 import com.sos.jitl.dailyplan.db.Calendar2DB;
+import com.sos.jitl.jobstreams.Constants;
+import com.sos.jitl.jobstreams.classes.JobStreamScheduler;
 import com.sos.joc.model.common.NameValuePair;
 import com.sos.joc.model.jobstreams.JobStreamJob;
 import com.sos.joc.model.jobstreams.JobStreamStarter;
 import com.sos.joc.model.jobstreams.JobStreamStarters;
+import com.sos.joc.model.joe.schedule.RunTime;
 
 public class DBLayerJobStreamStarters {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DBLayerJobStreamStarters.class);
+    private static final String MAX_DATE = "01-01-2038";
+
     private static final String DBItemJobStreamStarter = DBItemJobStreamStarter.class.getSimpleName();
     private final SOSHibernateSession sosHibernateSession;
 
@@ -117,8 +128,42 @@ public class DBLayerJobStreamStarters {
         return dbItemJobStreamStarter.getId();
     }
 
-    public void deleteInsert(JobStreamStarters jobStreamStarters) throws Exception {
+    
+    public Date getNextStartTime(ObjectMapper objectMapper, String timeZone, String runTimeString) throws JsonParseException, JsonMappingException, JsonProcessingException, IOException, Exception {
+        JobStreamScheduler jobStreamScheduler = new JobStreamScheduler(timeZone);
+        RunTime runTime=null;
+        if (runTimeString != null) {
+            runTime = objectMapper.readValue(runTimeString, RunTime.class);
+        }
+        if (runTime != null) {
+            Date from = new Date();
+            Date to = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+            String max = MAX_DATE;
+            Date maxDate = formatter.parse(max);
+            Calendar c = Calendar.getInstance();
 
+            do {
+                jobStreamScheduler.schedule(from, to, runTime, true);
+                c.setTime(to);
+                c.add(Calendar.MONTH, 1);
+                from = to;
+                to = c.getTime();
+            } while (jobStreamScheduler.getListOfStartTimes().isEmpty() && to.before(maxDate));
+        }
+        
+        Date now = new Date();
+        if (jobStreamScheduler.getListOfStartTimes() != null) {
+            for (Long start : jobStreamScheduler.getListOfStartTimes()) {
+                if (start > now.getTime()) {
+                    return new Date(start);
+                }
+            }
+        }
+        return null;
+    }
+ 
+    public void deleteInsert(JobStreamStarters jobStreamStarters, String timezone) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         DBLayerJobStreamHistory dbLayerJobStreamHistory = new DBLayerJobStreamHistory(sosHibernateSession);
         DBLayerJobStreamParameters dbLayerJobStreamParameters = new DBLayerJobStreamParameters(sosHibernateSession);
@@ -153,9 +198,13 @@ public class DBLayerJobStreamStarters {
             dbItemJobStreamStarter.setJobStream(jobStreamStarters.getJobStreamId());
             dbItemJobStreamStarter.setTitle(jobStreamStarter.getTitle());
             dbItemJobStreamStarter.setState(jobStreamStarter.getState());
+            
             if (jobStreamStarter.getRunTime() != null) {
                 dbItemJobStreamStarter.setRunTime(objectMapper.writeValueAsString(jobStreamStarter.getRunTime()));
             }
+            dbItemJobStreamStarter.setNextStart(getNextStartTime(objectMapper,timezone, dbItemJobStreamStarter.getRunTime()));
+            
+
             sosHibernateSession.save(dbItemJobStreamStarter);
             Long newId = dbItemJobStreamStarter.getId();
 
