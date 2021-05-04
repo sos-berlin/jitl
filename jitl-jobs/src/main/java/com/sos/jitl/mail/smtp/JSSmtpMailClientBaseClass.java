eb79ef9a101967e7f2265c1d9f7f8887f8d15d71
uwe-risse-sos <uwe.risse@sos-berlin.com>
2021-05-04 15:29:49 +0200

    JITL-653, JITL-654

--- jitl-jobs/src/main/java/com/sos/jitl/mail/smtp/JSSmtpMailClientBaseClass.java
+++ jitl-jobs/src/main/java/com/sos/jitl/mail/smtp/JSSmtpMailClientBaseClass.java
@@ -1,6 +1,8 @@
 package com.sos.jitl.mail.smtp;
 
 import sos.scheduler.job.JobSchedulerJobAdapter;
+import sos.spooler.Job_chain;
+import sos.spooler.Job_chain_node;
 import sos.spooler.Order;
 
 public class JSSmtpMailClientBaseClass extends JobSchedulerJobAdapter {
@@ -10,23 +12,7 @@ public class JSSmtpMailClientBaseClass extends JobSchedulerJobAdapter {
     protected JSSmtpMailClient objR = null;
     protected JSSmtpMailOptions objO = null;
 
-    private void addParameters() {
-        if (spooler_task != null) {
-            setJobId(spooler_task.id());
-        }
-        if (spooler_job != null) {
-            String jobName = spooler_job.name();
-
-            setJobName(jobName);
-            setJobFolder(spooler_job.folder_path());
-            setJobTitle(spooler_job.title());
-        }
-        this.getSchedulerParameters().put("SCHEDULER_JOB_NAME", spooler_job.name());
-        this.getSchedulerParameters().put("SCHEDULER_JOB_TITLE", spooler_job.title());
-        this.getSchedulerParameters().put("SCHEDULER_TASK_ID", String.valueOf(spooler_task.id()));
-
-    }
-
+   
     protected void createOptions(Order order, final String pstrEntryPointName) throws Exception {
         objR = new JSSmtpMailClient();
         objO = objR.getOptions();
@@ -35,7 +21,6 @@ public class JSSmtpMailClientBaseClass extends JobSchedulerJobAdapter {
         String strStepName = getCurrentNodeName(order, false);
         objO.setCurrentJobId(getJobId()).setCurrentJobName(getJobName()).setCurrentJobFolder(getJobFolder()).setCurrentNodeName(strStepName);
         objO.setAllOptions(getSchedulerParameterAsProperties(order));
-        addParameters();
     }
 
     protected void doProcessing(Order order) throws Exception {
